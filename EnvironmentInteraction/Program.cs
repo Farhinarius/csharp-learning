4eba9a7c391331d22f494f95a3832d685df7da7b
Farhinarius <farhat3027@gmail.com>
2021-08-28 15:51:52 +0300

    Environment interaction program

---
+++ EnvironmentInteraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnvironmentInteraction
+{
+    class Program
+    {
+        private static int Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine("Arg: {0}", arg);
+            }
+            
+            ShowEnvironmentDetails();
+            return 0;
+        }
+
+        private static void ShowEnvironmentDetails()
+        {
+            foreach (var drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive: {0}", drive);    
+            }
+            
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            
+            Console.WriteLine(".NET version: {0}", Environment.Version);
+        }
+    }
+}
